ed1e769eda2d9cbe2bb68cf48242810855f060c1
Jason <jiliangnghiem123@gmail.com>
2024-10-30 14:38:54 +0000

    Testing, takes a prompt and prints llama's response

--- model/main.py
+++ model/main.py
@@ -2,15 +2,40 @@
 This python file is for experimenting with models using transformers
 package from hugging face
 """
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-pipe = pipeline(
-    "text-generation", 
-    model="meta-llama/Llama-3.2-1B",
-    device=device,
-    max_length=20
-    )
+model_id = "meta-llama/Llama-3.2-1B"
 
-print(pipe('hello world'))
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4"
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="auto",
+    quantization_config=bnb_config
+)
+
+generator = pipeline(
+    "text-generation", 
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=128,
+    truncation=True,
+)
+
+def get_response(prompt):
+    response = generator(prompt)
+    gen_text = response[0]['generated_text']
+    return gen_text
+
+prompt = "What is Python"
+llama_response = get_response(prompt)
+print(llama_response)
