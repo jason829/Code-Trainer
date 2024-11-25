7fe46e1262bb96231a0d636c3d09f31fc2ee9e5b
Jason <jiliangnghiem123@gmail.com>
2024-11-25 12:36:45 +0000

    Chat templates added (meta-llama/Llama-3.2-1B-Instruct or compatible models ONLY).

--- model/main.py
+++ model/main.py
@@ -3,12 +3,15 @@ This python file is for experimenting with models using transformers
 package from hugging face
 """
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
-import torch
+import torch 
+# I need to down version torch for unsloth (fine tuning)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_id = "meta-llama/Llama-3.2-1B"
+model_id = "meta-llama/Llama-3.2-1B-Instruct" # meta-llama/Llama-3.2-3B-Instruct
 
-bnb_config = BitsAndBytesConfig(
+tokenizer = AutoTokenizer.from_pretrained(model_id, device=device)
+
+""" bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type="nf4"
@@ -21,21 +24,32 @@ model = AutoModelForCausalLM.from_pretrained(
     model_id,
     device_map="auto",
     quantization_config=bnb_config
-)
+) """
 
 generator = pipeline(
-    "text-generation", 
-    model=model,
-    tokenizer=tokenizer,
-    max_new_tokens=128,
+    "text-generation",
+    model=model_id,
     truncation=True,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
 )
 
-def get_response(prompt):
-    response = generator(prompt)
-    gen_text = response[0]['generated_text']
-    return gen_text
+tokenizer.chat_template
 
-prompt = "What is Python"
-llama_response = get_response(prompt)
-print(llama_response)
+prompt = "Create a function that takes 2 integer parameters and returns the sum in Python."
+better_prompt = [
+    {"role": "system", "content": "You are a Python programmer that teaches Python at an introductory level"},
+    {"role": "user", "content": prompt}
+]
+
+tokenizer.apply_chat_template(better_prompt, tokenize=True, add_generation_prompt=False)
+
+outputs = generator(
+    better_prompt,
+    max_new_tokens=256,
+    )
+
+# response = generator(better_prompt)
+gen_text = outputs[0]["generated_text"][-1]
+print(gen_text)
+
